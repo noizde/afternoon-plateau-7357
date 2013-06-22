32bf9a2c3486dcc14a3ead99dd61938125d5153b
Albert Dizon <akdizon@gmail.com>
2013-06-22 23:17:41 +0800

    angular test

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,135 @@
+function AreaCtrl($scope) {
+
+  $scope.areasNationwide = {
+    "name": "Nationwide",
+    "branches": [{
+        "name": "Human Nature",
+        "text": "Our super pro-philippines, pro-poor, pro-environment partner"
+      },{
+        "name": "Chicken Charlie",
+        "text": "Must. Absolutely. Try. That Signature Burger."
+      },{
+        "name": "The Frap Bar",
+        "text": "Rumor has it the Fraps are just as good, if not better than the iced tea."
+      },
+    ]
+  }
+
+  $scope.areasLuzon = {
+    "name": "Luzon",
+    "branches": [{
+        "name": "Hill Station (Baguio",
+        "text": "Recognized as one of the best restos in Asia by the international Miele Guide. Twice. ‘nuf said."
+      },{
+        "name": "Hungry Ant (Baguio)",
+        "text": "Go to the cafe where the sweet tooth leads you"
+      },{
+        "name": "GK Enchanted Farm (Bulacan)",
+        "text": "But of course! An incubator of social enterprises."
+      },{
+        "name": "Circle Hostel (La Union and Zambales)",
+        "text": "Budget-friendly. Check. Eco-friendly. Check. Artsy/Adventurous Sanctuary. Check. Cool friendly people. Check. Then why go anywhere else when the surf’s up?"
+      },{
+        "name": "Café Antonio (Los Banos)",
+        "text": "A culinary indulgence smack in the birthplace of our national hero. The Doc would be proud their serving his favourite brew."
+      }
+    ]
+  }
+
+  $scope.areasVisayas = {
+    "name": "Visayas",
+    "branches": [{
+        "name": "Café Bob’s (Bacolod)",
+        "text": "Aside from our brews, we heard they also serve Bacolod craft beer too."
+      },{
+        "name": "The Chillage (Cebu)",
+        "text": "“Shop. Eat. Play.” is what they say. Just a super chill joint to check out when in the city."
+      },{
+        "name": "The Henry Hotel (Cebu)",
+        "text": "Fun. Funky. Fabulous."
+      }
+    ]
+  }
+
+  $scope.areasMetro = [
+    {
+      "name": "Makati",
+      "branches": [{
+        "name": "Commune (Dela Costa St)",
+        "text": "A coffee commune. Tune in for their happy hours too. Or better yet, just order iced tea anytime!"
+      },{
+        "name": "Cucina Andare (Glorietta Park)",
+        "text": "Open Fri to Sun nights! Cool food trucks transport you anywhere your stomach hankers for with their culinary creativity (disclaimer: the trucks do not actually move)"
+      },{
+        "name": "Juju Eats (Pasong Tamo Extension)",
+        "text": "A new spin on salad. Eating this healthy has never been this yummy."
+      },{
+        "name": "Fairmont Hotel",
+        "text": "Naks, ang sosy."
+      },{
+        "name": "Anton Guerrero (+63917-884-4111)",
+        "text": " Reseller of everything hard to find. But first you have to find him."
+      }]
+    },{
+      "name": "The Fort/BGC",
+      "branches": [{
+        "name": "Slice",
+        "text": "Choco yema, choco stirrer, mochamisu, and oh look, BAYANI BREW! Head to Slice on High St and try the new addition to their menu!"
+      },{
+        "name": "Wrong Ramen",
+        "text": "Seems odd isn’t it? But pairing BB with this rebellious Jap fusion food makes it oh soooo right!"
+      },{
+        "name": "Casa Marcos",
+        "text": "Old meets the new brew. Have gambas, steak ala pobre, or maybe the sopa de mariscos. Such well-loved Spanish dishes pair up real nicely with Bayani Brew. Si? Si!"
+      },{
+        "name": "Manos Nail Lounge",
+        "text": "Yes, Bayani Brew is best enjoyed while having a foot spa. And with pink nail polish to boot."
+      },{
+        "name": "Midnight Mercato",
+        "text": "Your ultimate go-to for your midnight munchies on late night Fridays/Saturdays or right after stalking the Azkals/Younghusbands during soccer practice (what? They don’t play there?)"
+      }]
+    },{
+      "name": "Quezon City",
+      "branches": [{
+        "name": "GK Enchanted Farm Cafe (Commonwealth)",
+        "text" : "The watering hole for social entrepreneurs. Then visit the Human Nature flagship store downstairs."
+      },{
+        "name": "ElarZ (Speaker Perez and soon other kiosks)",
+        "text" : "After pigging out on the pambansang lechon, drinking copious amounts of Bayani Brew ensures your cholesterol stays low."
+      },{
+        "name": "Elorde Boxing Gym (Katipunan Ave & Don Antonio Ave)",
+        "text" : "After that knock-out punch, nothing’s better than gulping down an ice-cold Bayani Brew."
+      },{
+        "name": "Mario’s (Tomas Morato)",
+        "text" : "t’s the longest running resto on the block. Dine there and you’ll find out why."
+      },{
+        "name": "Mezza Norte Trinoma",
+        "text" : "Ultimate eats at the north every Friday, Sat and Sunday!"
+      },{
+        "name": "Nomama (Scout Tuason)",
+        "text" : "Not your mama’s ramen also serves not your ordinary iced tea."
+      }]
+    },{
+      "name": "Greenhills",
+      "branches": [{
+        "name": "The Appraisery",
+        "text" : "Great concept! Vintage fashion meets yummy food. And while snacking and shopping, have a nice chit-chat with the store owner/bartender."
+      }]
+    },{
+      "name": "Kapitolyo",
+      "branches": [{
+        "name": "CAB Café",
+        "text" : "Negrenses Unite!"
+      },{
+        "name": "Edgy Veggy",
+        "text" : "Being vegetarian can be this exciting and delicious!"
+      }]
+    },{
+      "name": "Manila",
+      "branches": [{
+        "name": "Mabuhay Restop (Luneta)",
+        "text" : "Travel. Cafe. Museum. Experience the Philippines."
+      }]
+    }
+  ];
+}
